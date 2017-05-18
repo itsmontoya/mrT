029ce4caf57e42fb8a7112e7fd2f608db73884a2
itsMontoya <josh@montoya.io>
2017-05-18 12:59:02 -0700

    Adding mapDB example

---
+++ examples/mapDB/mapDB.go
@@ -0,0 +1,151 @@
+package mapDB
+
+import (
+	"sync"
+
+	"github.com/itsmontoya/mrT"
+	"github.com/missionMeteora/toolkit/errors"
+)
+
+const (
+	// ErrKeyDoesNotExist is returned when a key does not exist within the map
+	ErrKeyDoesNotExist = errors.Error("key does not exist")
+)
+
+// New will return a new map db
+func New(dir, name string) (mp *MapDB, err error) {
+	var m MapDB
+	// Initialize map
+	m.m = make(map[string]string)
+	// Create a new instance of mrT
+	if m.mrT, err = mrT.New(dir, name); err != nil {
+		return
+	}
+
+	m.mrT.ForEach(func(lineType byte, key, value []byte) (end bool) {
+		switch lineType {
+		case mrT.PutLine:
+			m.m[string(key)] = string(value)
+		case mrT.DeleteLine:
+			delete(m.m, string(key))
+		}
+
+		return
+	})
+
+	// Assign pointer to our MapDB
+	mp = &m
+	return
+}
+
+// MapDB is a simple map database
+type MapDB struct {
+	// Mutex for thread-safety
+	mux sync.RWMutex
+	// Internal map store
+	m map[string]string
+	// Our backend-storage
+	mrT *mrT.MrT
+	// Closed state
+	closed bool
+}
+
+// Get will retrieve a value from the DB
+func (m *MapDB) Get(key string) (value string, err error) {
+	var ok bool
+	m.mux.RLock()
+	defer m.mux.RUnlock()
+
+	if m.closed {
+		err = errors.ErrIsClosed
+		return
+	}
+
+	if value, ok = m.m[key]; !ok {
+		err = ErrKeyDoesNotExist
+		return
+	}
+
+	return
+}
+
+// Put will set a value for a given key within the DB
+func (m *MapDB) Put(key, value string) (err error) {
+	m.mux.Lock()
+	defer m.mux.Unlock()
+
+	if m.closed {
+		err = errors.ErrIsClosed
+		return
+	}
+
+	if err = m.mrT.Txn(func(txn *mrT.Txn) (err error) {
+		txn.Put([]byte(key), []byte(value))
+		return
+	}); err != nil {
+		return
+	}
+
+	m.m[key] = value
+	return
+}
+
+// Delete will remove a value for a given key within the DB
+func (m *MapDB) Delete(key string) (err error) {
+	var ok bool
+	m.mux.Lock()
+	defer m.mux.Unlock()
+
+	if m.closed {
+		err = errors.ErrIsClosed
+		return
+	}
+
+	if _, ok = m.m[key]; !ok {
+		return ErrKeyDoesNotExist
+	}
+
+	if err = m.mrT.Txn(func(txn *mrT.Txn) (err error) {
+		txn.Delete([]byte(key))
+		return
+	}); err != nil {
+		return
+	}
+
+	delete(m.m, key)
+	return
+}
+
+// ForEach will iterate through all the values within the DB
+func (m *MapDB) ForEach(fn ForEachFn) (ended bool) {
+	m.mux.RLock()
+	defer m.mux.RUnlock()
+	// Iterate through all the map values
+	for key, value := range m.m {
+		if ended = fn(key, value); ended {
+			return
+		}
+	}
+
+	return
+}
+
+// Close will close map db
+func (m *MapDB) Close() (err error) {
+	m.mux.Lock()
+	defer m.mux.Unlock()
+	if m.closed {
+		err = errors.ErrIsClosed
+		return
+	}
+
+	err = m.mrT.Close()
+
+	m.closed = true
+	m.m = nil
+	m.mrT = nil
+	return
+}
+
+// ForEachFn is used for iterating over map db items
+type ForEachFn func(key, value string) (end bool)
---
+++ examples/mapDB/mapDB_test.go
@@ -0,0 +1,64 @@
+package mapDB
+
+import (
+	"testing"
+)
+
+func TestMapDB(t *testing.T) {
+	var (
+		m   *MapDB
+		err error
+
+		entryCnt int
+	)
+
+	if m, err = New("db", "testing"); err != nil {
+		t.Fatal(err)
+	}
+
+	m.Put("greeting", "Hello")
+	m.Put("results", "none")
+	m.Put("name", "world")
+	m.Delete("results")
+	m.Put("name", "John Doe")
+
+	tm := map[string]string{
+		"greeting": "Hello",
+		"name":     "John Doe",
+	}
+
+	m.ForEach(func(key, value string) (end bool) {
+		if tm[key] != value {
+			t.Fatalf("invalid value, expected %s and received %s", tm[key], value)
+		}
+
+		entryCnt++
+		return
+	})
+
+	if entryCnt != 2 {
+		t.Fatalf("invalid entry count, expected %d and received %d", 2, entryCnt)
+	}
+
+	if err = m.Close(); err != nil {
+		t.Fatal(err)
+	}
+
+	if m, err = New("db", "testing"); err != nil {
+		t.Fatal(err)
+	}
+
+	entryCnt = 0
+	m.ForEach(func(key, value string) (end bool) {
+		if tm[key] != value {
+			t.Fatalf("invalid value, expected %s and received %s", tm[key], value)
+		}
+
+		entryCnt++
+		return
+	})
+
+	if entryCnt != 2 {
+		t.Fatalf("invalid entry count, expected %d and received %d", 2, entryCnt)
+	}
+}
