bd87e81e057f6112456e2c50a730b147d75bbb10
itsMontoya <josh@montoya.io>
2017-10-03 14:16:43 -0700

    Update shasher to PathDNA

--- mrT.go
+++ mrT.go
@@ -10,7 +10,7 @@ import (
 	"strings"
 	"sync"
 
-	"github.com/Path94/fileutils/shasher"
+	"github.com/PathDNA/fileutils/shasher"
 	"github.com/itsmontoya/async/file"
 	"github.com/itsmontoya/middleware"
 	"github.com/itsmontoya/seeker"
